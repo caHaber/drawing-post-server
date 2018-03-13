483852363c3fd5384eb5a5076df7c6d732f4913c
Casey Haber <cahaber@dons.usfca.edu>
2018-03-13 12:50:32 -0700

    REgex error

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/', function(req, res){
     console.log(req.headers.referer);
     
     var name = URLregex.exec(req.headers.referer)
-    name = name.substring(1);;
+    name = name[0].substring(1);;
     // console.log(dir);
 
     fs.writeFile('./'+ name + '/' + new Date().getTime() + name + '-.json', JSON.stringify(req.body), function(err) {     
