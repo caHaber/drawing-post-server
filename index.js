0d60f382e245c70d75d27d11af51581c09baa5df
Casey Haber <cahaber@dons.usfca.edu>
2018-03-13 20:34:29 -0700

    Added sqlite server and sketch table

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ var app = express();
 var bodyParser = require('body-parser');
 var async = require('async');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database/sketches.db');
+
 app.use(bodyParser.json());
 
-//app.use(express.bodyParser());
+var total;
 
+db.all('SELECT * FROM MAIN',function(err,rows){
+    console.log(rows.length);
+    total = rows.length;
+    console.log(err);
+});
 
 app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -15,58 +23,40 @@ app.all('/', function(req, res, next) {
     next();
 });
 
-var folder = './casey/';
 var filenames = [];
 
 app.get('/', function(req, res){
      console.log('GET /')
-     var html = '<html><body><h1> From rpi!!!!</h1></body></html>';
+     var html = '<html><body><h1> Error!!!!</h1></body></html>';
      res.writeHead(200, {'Content-Type': 'text/html'});
-     switch(req.query.p){
-        case "casey": 
-            folder = './casey/'
-            break;
-        case "alark": 
-            folder = './alark/'
-            break;
-        case "noopur": 
-            folder = './noopur/'
-            break;
-        case "dom": 
-            folder = './dom/'
-            break;     
-     }
+     var person = req.query.p;   
 
-     filenames = [];
-     fs.readdirSync(folder).forEach(file => {
-        filenames.push(folder + file);
-        console.log(file, "File");
-      })
+     var statement = 'SELECT * FROM MAIN where NAME=?';
 
-     async.eachSeries(
-        // Pass items to iterate over
-        filenames,
-        // Pass iterator function that is called for each item
-        function(filename, cb) {
-          fs.readFile(filename, function(err, content) {
-            if (!err) {
-              res.write(content);
-            }
-            // Calling cb makes it go to the next item.
-            cb(err);
-          });
-        },
-        // Final callback after each item has been iterated over.
-        function(err) {
-          res.end()
-        //   res.end(html);
+    db.all(statement, person, function(err,rows){
+        if(err) {
+            res.write(html);
+            console.log(err);
+            res.end();
+        } 
+        else {
+            console.log(rows);
+            rows.forEach(function(d){
+                
+                if(d.Sketch != null){
+                    // console.log(JSON.parse(d.Sketch));
+                    res.write(d.Sketch);
+                }
+                   
+            });   
+            res.end();
         }
-      );
-     
+        
+    });
+   
 });
 
 const URLregex = /-.+[^//]/;
-
 
 app.post('/', function(req, res){
     //TODO check for known fingerprint (me | Dom | alark | noopur)
@@ -76,12 +66,29 @@ app.post('/', function(req, res){
     var name = URLregex.exec(req.headers.referer)
     name = name[0].substring(1);;
 
-    fs.writeFile('./'+ name + '/' + new Date().getTime() + name + '.json', JSON.stringify(req.body), function(err) {     
-        if(err) {
-            return console.log(err);
-        }  
-        console.log("The file was saved!");
-    }); 
+    total++;
+
+    var Sketch,width,height,strokewidth,color;
+
+    // console.log(r)
+    Sketch = JSON.stringify(req.body.lineSegs);
+    width = req.body.width;
+    height = req.body.height;
+    strokewidth = req.body.strokeWidth;
+    color = req.body.strokeColor.toString();
+    
+    try {
+        db.run("INSERT INTO MAIN VALUES(?,?,?,?,?,?,?)", [
+            total,
+            name,
+            Sketch,
+            width,
+            height,
+            strokewidth,
+            color]);
+    }catch(err){
+        console.log(err);
+    }
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('thanks');
